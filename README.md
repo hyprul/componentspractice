9203e4069800381d837e50f03ead02f5e94e68d6
Sean Huang <hyprul@ucla.edu>
2019-11-28 19:49:24 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# componentspractice
-Created with CodeSandbox
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Heading from "./Heading";
+
+export default function App() {
+  return <Heading />;
+}
